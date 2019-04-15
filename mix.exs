0ef291c63ae7e877f09a3d29478a00c360fae3fd
Amanda Dolan <amandacrawfordalc@gmail.com>
2019-04-14 23:29:37 -0400

    Just remove the ecto.create from the alias test task

--- mix.exs
+++ mix.exs
@@ -56,8 +56,7 @@ defmodule Pomo.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"],
-      ci_test: ["ecto.migrate", "test"],
+      test: ["ecto.migrate", "test"],
       release: ["ecto.migrate"]
     ]
   end
