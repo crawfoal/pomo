cc848fb2a35d2576bce856d7b038b681d39f0f17
Amanda Dolan <amandacrawfordalc@gmail.com>
2019-04-14 21:25:10 -0400

    It helps to use the correct app name... :)

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :pomo, PomoWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   http: [port: System.get_env("PORT")]
 
-config :hello, Hello.Repo,
+config :pomo, Pomo.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
